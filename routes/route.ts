2d09fd7487da64e5b986b28f95257913bd771a0a
Mahib Khan <khanmahibalam@gmail.com>
2026-05-19 16:06:56 +0600

    All route

--- routes/route.ts
+++ routes/route.ts
@@ -1,19 +1,16 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { productControler } from "../constroler/productControl";
 
-export const  routeHandeler = (req :IncomingMessage, res : ServerResponse) => {
+export const routeHandeler = (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url;
   const method = req.method;
   if (url === "/" && method === "GET") {
-    // console.log("Received a GET request to the root path");
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "This is root path" }));
-  }
-  else if (url?.startsWith("/products") && method === "GET") {
+  } else if (url?.startsWith("/products") && method === "GET") {
     productControler(req, res);
-  }
-  else{
+  } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Not Found" }));
   }
-}
+};

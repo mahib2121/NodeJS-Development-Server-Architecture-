56a09d6962c85e75a721a86c539e94c3c2165318
Mahib Khan <khanmahibalam@gmail.com>
2026-05-19 16:08:12 +0600

    update

--- server.ts
+++ server.ts
@@ -1,22 +1,9 @@
 import { log } from "console";
 import { createServer, IncomingMessage, Server } from "http";
+import { routeHandeler } from "./routes/route";
 
-const server : Server = createServer((req : IncomingMessage , res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/" && method === "GET") {
-    // console.log("Received a GET request to the root path");
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "This is root path" }));
-  }
-  else if (url?.startsWith("/products") && method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "This is All products " }));
-  }
-  else{
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: "Not Found" }));
-  }
+const server: Server = createServer((req: IncomingMessage, res) => {
+  routeHandeler(req, res);
 });
 
 server.listen(3000, () => {
